423ae708cf8f019d76fbe86e4f8e693971b14725
VISWANATH78 <VICHU110602@GMAIL.COM>
2021-12-20 13:59:18 +0530

    code

---
+++ Amazon Gift Code Generator/main.py
@@ -0,0 +1,29 @@
+import random
+import time
+def g(choice):
+    Data = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+    result = ""
+    while choice >= 1:
+        c = random.choice(Data)
+        result = result + c
+        choice=choice-1
+    return result
+print("How many codes to be genrated ?")
+number = input(">")
+print("")
+n = int(number)
+n = int(number)
+text = input("Type Amazon to get codes -")
+t = text.lower()
+if text == "Amazon":
+    for i in range(n):
+        print(g(4)+"-"+g(6)+"-"+g(5))
+
+else:
+    print("u have typed wrong type like this Amazon")
+
+
+time.sleep(2);
+
+
+
